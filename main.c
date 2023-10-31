affc8a2b651b8bb2969b326a6d76c59bc9b4658e
jackhu <jackhu.521.rose@gmail.com>
2023-10-31 12:44:04 +0800

    cmp

--- main.c
+++ main.c
@@ -7,6 +7,12 @@ int addi_ins_2(int a);
 int addi_reg(int a, int b);
 int sub_reg(int a, int b);
 
+int slti_ins(int a);
+int sltiu_ins(int a);
+
+int slt_reg(int a, int b);
+int sltu_reg(int a, int b);
+
 int main()
 {
     int res = 0;
@@ -21,6 +27,24 @@ int main()
 
     res = sub_reg(4, 5);
     assert(res == -1);
+
+    res = slti_ins(4); // 4 < 5? 
+    assert(res == 1);
+
+    res = slti_ins(6); // 6 < 5?
+    assert(res == 0);
+
+    res = sltiu_ins(2048); // 2048 < unsigned(-100)=0xffffff7c?
+    assert(res == 1);
+
+    res = slt_reg(4, 5); // 4 < 5?
+    assert(res == 1);
+
+    res = slt_reg(-100, -99);
+    assert(res == 1);
+
+    res = sltu_reg(-100, -99); // unsigned(-100) 7c < unsigned(-99) 7d?
+    assert(res == 1);
 
     printf("all assertion passed\n");
     return 0;
